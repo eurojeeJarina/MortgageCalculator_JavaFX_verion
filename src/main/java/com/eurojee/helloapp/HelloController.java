c8ce31328566ae240e8e7ef8c921e668e143a28e
Eurojee Jarina <44507657+eurojeeJarina@users.noreply.github.com>
2021-11-02 00:49:17 +0000

    JAVAFX version of the mortgage calculator still a work in progress

--- src/main/java/com/eurojee/helloapp/HelloController.java
+++ src/main/java/com/eurojee/helloapp/HelloController.java
@@ -1,11 +1,12 @@
 package com.eurojee.helloapp;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.paint.Color;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class HelloController {
@@ -18,26 +19,60 @@ public class HelloController {
     TextField termLoanField;
 
     @FXML
+    Label totalResultText;
+
+    @FXML
     protected void onHelloButtonClick() {
         //System.out.println("onHelloButtonCLick called!");
         if (!loanAmountField.getText().trim().isEmpty()
                 && !interestRateField.getText().trim().isEmpty()
                 && !termLoanField.getText().trim().isEmpty()) {
-            System.out.println("All fields are not empty");
-        } else {
 
-            if (loanAmountField.getText() == null || loanAmountField.getText().trim().isEmpty()) {
-             System.out.println("LoanAmountField is Empty!");
-            }else if(interestRateField.getText() == null || interestRateField.getText().trim().isEmpty()){
-                System.out.println("Interest Rate Field is Empty!");
-            }else if(termLoanField.getText() == null || termLoanField.getText().trim().isEmpty()){
-                System.out.println("Loan Term Field is Empty!");
+            try {
+                double loanAmount = Double.parseDouble(loanAmountField.getText());
+                double interestRate = Double.parseDouble(interestRateField.getText());
+                int yearsOfLoan = Integer.parseInt(termLoanField.getText());
+
+                //System.out.println(monthlyRepayments(loanAmount, interestRate, yearsOfLoan));
+                totalResultText.setText(monthlyRepayments(loanAmount, interestRate, yearsOfLoan));
+            } catch (NumberFormatException e) {
+
+                totalResultText.setText("Only numbers are allowed");
+                totalResultText.setStyle("-fx-text-fill: red; -fx-font-size: 16px;");
             }
+
+        } else {
+            totalResultText.setText("Missing fields.");
+            totalResultText.setStyle("-fx-text-fill: red; -fx-font-size: 16px;");
         }
     }
 
-    private void validation(){
+    @FXML
+    protected void resetForm(){
+        loanAmountField.setText("");
+        interestRateField.setText("");
+        termLoanField.setText("");
+        totalResultText.setText("\u20ac0 monthly");
+        totalResultText.setStyle(null);
+    }
 
+    private String monthlyRepayments(double loan, double ratesPercentage, int years){
+
+
+        int yearsToMonthsConversion = years * 12; // lets convert years into months
+        double percentToDecimalConversion = (ratesPercentage / 100) / 12; // convert percentage into decimals
+
+        double loanAmount = loan; // the main amount borrowed
+        double interestRate = percentToDecimalConversion;
+        int numberOfPayments = yearsToMonthsConversion;
+
+        double topEquation = (Math.pow((1 + interestRate), numberOfPayments)) * interestRate; // top of the equation
+        double bottomEquation = (Math.pow((1 + interestRate) , numberOfPayments) - 1); // bottom of the equation
+        double totalAmountOfRepayment = (topEquation / bottomEquation) * loanAmount;
+
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        return "\u20ac" + df.format(totalAmountOfRepayment) + " Monthly";
     }
 
 
